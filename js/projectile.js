cad53482756c2a493372b5a3ca9404baa129401c
Ari MB <arimb1999@gmail.com>
2022-08-01 01:57:25 +0300

    projectile target height arrows

--- js/projectile.js
+++ js/projectile.js
@@ -1,126 +1,142 @@
 $(document).ready(function(){
 
-    $("input#drag-enable").change(() => { $("input#drag-enable").parent().nextAll().children("input,select").attr("disabled", !$("input#drag-enable").prop("checked")); });
-
-    $("input, select").change(function(){
-        var h0, v0, theta0, end_fcn, output
-        if ($("input#target-distance-driven").prop("checked")) {
-            $(".target-height-driven, .initial-driven").find("*").addBack().attr("disabled", false);
-            $(".target-distance-driven").find("*").addBack().attr("disabled", true);
-            
-            // solve for target distance and angle
-            h0 = parseFloat($("input#initial_height").val() * $("select#initial_height-units").val());
-            v0 = parseFloat($("input#initial_vel").val() * $("select#initial_vel-units").val());
-            theta0 = parseFloat($("input#initial_angle").val()) * Math.PI/180;
-            var hf = $("input#target_height").val() * $("select#target_height-units").val();
-            end_fcn = $("input#target_height-direction").prop("checked") ? `v[1]>=0 && x[1]>=${hf}` : `v[1]<=0 && x[1]<=${hf}`;
-            output = simulate(h0, v0, theta0, end_fcn);
-
-            if (Math.abs(output[0][output[0].length-1][1] - hf) <= 0.05) {
-                $("input#target_distance").val( +((output[0][output[0].length-1][0] / $("select#target_distance-units").val()).toFixed(3)) );
-                $("input#target_angle").val( +((Math.atan2(output[1][1], output[1][0]) * 180/Math.PI).toFixed(2)) );    
-            } else {
-                $("input#target_distance, input#target_angle").val("");
-            }
-        } else if ($("input#target-height-driven").prop("checked")) {
-            $(".initial-driven, .target-distance-driven").find("*").addBack().attr("disabled", false);
-            $(".target-height-driven").find("*").addBack().attr("disabled", true);
-            
-            // solve for target height and angle
-            h0 = parseFloat($("input#initial_height").val() * $("select#initial_height-units").val());
-            v0 = parseFloat($("input#initial_vel").val() * $("select#initial_vel-units").val());
-            theta0 = parseFloat($("input#initial_angle").val()) * Math.PI/180;
-            end_fcn = `x[0] >= ${$("input#target_distance").val() / $("select#target_distance-units").val()}`;
-            output = simulate(h0, v0, theta0, end_fcn);
-
-            $("input#target_height").val( +((output[0][output[0].length-1][1] / $("select#target_height-units").val()).toFixed(3)) );
-            $("input#target_height-direction").prop("checked", output[1][1] >= 0);
-            $("input#target_angle").val( +((Math.atan2(output[1][1], output[1][0]) * 180/Math.PI).toFixed(2)) );
-        } else if ($("input#initial-driven").prop("checked")) {
-            $(".target-height-driven, .target-distance-driven").find("*").addBack().attr("disabled", false);
-            $(".initial-driven").find("*").addBack().attr("disabled", true);
-
-            var d = parseFloat($("input#target_distance").val() * $("select#target_distance-units").val());
-            var thetaf = parseFloat($("input#target_angle").val() * Math.PI/180);
-            h0 = parseFloat($("input#initial_height").val() * $("select#initial_height-units").val());
-            var hf = parseFloat($("input#target_height").val() * $("select#target_height-units").val());
-            
-            theta0 = Math.atan(2*(hf-h0)/d - Math.tan(thetaf));
-            v0 = Math.sqrt(9.8*d/Math.abs(Math.tan(theta0) - Math.tan(thetaf))) / Math.cos(theta0);
-            end_fcn = `x[0] >= ${$("input#target_distance").val() * $("select#target_distance-units").val()}`;
-            output = simulate(h0, v0, theta0, end_fcn);
-            console.log(output);
-
-            if (output[0].length==0 || Math.abs(output[0][output[0].length-1][0] - d) <= 0.05) {
-                $("input#initial_vel").val( +((v0 / $("select#initial_vel-units").val()).toFixed(2)) );
-                $("input#initial_angle").val( +((theta0 * 180/Math.PI).toFixed(2)) );
-            } else {
-                $("input#initial_vel, input#initial_angle").val("");
-            }
-        }
-
-        // Draw graph
-        var max = Math.max(Math.max.apply(null, output[0].map(x => x[0])), Math.max.apply(null, output[0].map(x => x[1]))) / $("select#initial_height-units").val();
-        if ($("input#obstacle-enable").prop("checked")) {
-            var obs_x = parseFloat($("input#obstacle_distance").val() * $("select#obstacle_distance-units").val());
-            var obs_y = parseFloat($("input#obstacle_height").val() * $("select#obstacle_height-units").val());
-        } else {
-            var obs_x = 0, obs_y = 0;
-        }
-
-        $("canvas#graph").remove();
-        if (output[0].length > 1) {
-            $("span.warn").hide();
-            $("div.graph").prepend('<canvas id="graph"></canvas>');
-            var graph = new Chart("graph", {
-                data: {
-                    labels: output[0].map(x => (x[0] / $("select#initial_height-units").val()).toFixed(2)),
-                    datasets: [{
-                        type: "line",
-                        data: output[0].map(x => x[1] / $("select#initial_height-units").val()),
-                        borderColor: "black",
-                        fill: false,
-                        pointRadius: 0
-                    },{
-                        type: "line",
-                        data: [{x: obs_x, y: 0}, {x: obs_x, y: obs_y}],
-                        borderColor: "red",
-                        fill: false,
-                        pointRadius: 0
-                    }]
-                },
-                options: {
-                    responsive: true,
-                    aspectRatio: 1,
-                    plugins: {
-                        legend: {display: false}
-                    },
-                    scales: {
-                        y: {
-                            position: "left",
-                            min: 0,
-                            max: max,
-                            ticks: {stepSize: 0.5, includeBounds: false}
-                        },
-                        x: {
-                            type: "linear",
-                            position: "bottom",
-                            min: 0,
-                            max: max,
-                            ticks: {stepSize: 0.5}
-                        }
-                    },
-                    animation: {
-                        duration: 0
-                    }
-                }
-            });
-        } else $("span.warn").show();
-        
+    $("input#drag-enable").change(() => { 
+        $("input#drag-enable").parent().nextAll().children("input,select").attr("disabled", !$("input#drag-enable").prop("checked"));
     });
+
+    $("input#target_height-direction").change(() => {
+        if ($("input#target_height-direction").prop("checked")){
+            $("img#up_arrow").show();
+            $("img#down_arrow").hide();
+        } else {
+            $("img#up_arrow").hide();
+            $("img#down_arrow").show();
+        }
+    }).change();
+    $("img#up_arrow").click(() => $("input#target_height-direction").prop("checked", false).change());
+    $("img#down_arrow").click(() => $("input#target_height-direction").prop("checked", true).change());
+    
+    $("input, select").change(update);
     $("input#initial_height").change();
 
 });
+
+function update(){
+    var h0, v0, theta0, end_fcn, output
+    if ($("input#target-distance-driven").prop("checked")) {
+        $(".target-height-driven, .initial-driven").find("*").addBack().attr("disabled", false);
+        $(".target-distance-driven").find("*").addBack().attr("disabled", true);
+        
+        // solve for target distance and angle
+        h0 = parseFloat($("input#initial_height").val() * $("select#initial_height-units").val());
+        v0 = parseFloat($("input#initial_vel").val() * $("select#initial_vel-units").val());
+        theta0 = parseFloat($("input#initial_angle").val()) * Math.PI/180;
+        var hf = $("input#target_height").val() * $("select#target_height-units").val();
+        end_fcn = $("input#target_height-direction").prop("checked") ? `v[1]>=0 && x[1]>=${hf}` : `v[1]<=0 && x[1]<=${hf}`;
+        output = simulate(h0, v0, theta0, end_fcn);
+
+        if (Math.abs(output[0][output[0].length-1][1] - hf) <= 0.05) {
+            $("input#target_distance").val( +((output[0][output[0].length-1][0] / $("select#target_distance-units").val()).toFixed(3)) );
+            $("input#target_angle").val( +((Math.atan2(output[1][1], output[1][0]) * 180/Math.PI).toFixed(2)) );    
+        } else {
+            $("input#target_distance, input#target_angle").val("");
+        }
+    } else if ($("input#target-height-driven").prop("checked")) {
+        $(".initial-driven, .target-distance-driven").find("*").addBack().attr("disabled", false);
+        $(".target-height-driven").find("*").addBack().attr("disabled", true);
+        
+        // solve for target height and angle
+        h0 = parseFloat($("input#initial_height").val() * $("select#initial_height-units").val());
+        v0 = parseFloat($("input#initial_vel").val() * $("select#initial_vel-units").val());
+        theta0 = parseFloat($("input#initial_angle").val()) * Math.PI/180;
+        end_fcn = `x[0] >= ${$("input#target_distance").val() * $("select#target_distance-units").val()}`;
+        output = simulate(h0, v0, theta0, end_fcn);
+
+        $("input#target_height").val( +((output[0][output[0].length-1][1] / $("select#target_height-units").val()).toFixed(3)) );
+        $("input#target_height-direction").prop("checked", output[1][1] >= 0);
+        $("input#target_angle").val( +((Math.atan2(output[1][1], output[1][0]) * 180/Math.PI).toFixed(2)) );
+    } else if ($("input#initial-driven").prop("checked")) {
+        $(".target-height-driven, .target-distance-driven").find("*").addBack().attr("disabled", false);
+        $(".initial-driven").find("*").addBack().attr("disabled", true);
+
+        var d = parseFloat($("input#target_distance").val() * $("select#target_distance-units").val());
+        var thetaf = parseFloat($("input#target_angle").val() * Math.PI/180);
+        h0 = parseFloat($("input#initial_height").val() * $("select#initial_height-units").val());
+        var hf = parseFloat($("input#target_height").val() * $("select#target_height-units").val());
+        
+        theta0 = Math.atan(2*(hf-h0)/d - Math.tan(thetaf));
+        v0 = Math.sqrt(9.8*d/Math.abs(Math.tan(theta0) - Math.tan(thetaf))) / Math.cos(theta0);
+        end_fcn = `x[0] >= ${$("input#target_distance").val() * $("select#target_distance-units").val()}`;
+        output = simulate(h0, v0, theta0, end_fcn);
+        console.log(output);
+
+        if (output[0].length==0 || Math.abs(output[0][output[0].length-1][0] - d) <= 0.05) {
+            $("input#initial_vel").val( +((v0 / $("select#initial_vel-units").val()).toFixed(2)) );
+            $("input#initial_angle").val( +((theta0 * 180/Math.PI).toFixed(2)) );
+        } else {
+            $("input#initial_vel, input#initial_angle").val("");
+        }
+    }
+
+    // Draw graph
+    var max = Math.max(Math.max.apply(null, output[0].map(x => x[0])), Math.max.apply(null, output[0].map(x => x[1]))) / $("select#initial_height-units").val();
+    if ($("input#obstacle-enable").prop("checked")) {
+        var obs_x = parseFloat($("input#obstacle_distance").val() * $("select#obstacle_distance-units").val());
+        var obs_y = parseFloat($("input#obstacle_height").val() * $("select#obstacle_height-units").val());
+    } else {
+        var obs_x = 0, obs_y = 0;
+    }
+
+    $("canvas#graph").remove();
+    if (output[0].length > 1) {
+        $("span.warn").hide();
+        $("div.graph").prepend('<canvas id="graph"></canvas>');
+        var graph = new Chart("graph", {
+            data: {
+                labels: output[0].map(x => (x[0] / $("select#initial_height-units").val()).toFixed(2)),
+                datasets: [{
+                    type: "line",
+                    data: output[0].map(x => x[1] / $("select#initial_height-units").val()),
+                    borderColor: "black",
+                    fill: false,
+                    pointRadius: 0
+                },{
+                    type: "line",
+                    data: [{x: obs_x, y: 0}, {x: obs_x, y: obs_y}],
+                    borderColor: "red",
+                    fill: false,
+                    pointRadius: 0
+                }]
+            },
+            options: {
+                responsive: true,
+                aspectRatio: 1,
+                plugins: {
+                    legend: {display: false}
+                },
+                scales: {
+                    y: {
+                        position: "left",
+                        min: 0,
+                        max: max,
+                        ticks: {stepSize: 0.5, includeBounds: false}
+                    },
+                    x: {
+                        type: "linear",
+                        position: "bottom",
+                        min: 0,
+                        max: max,
+                        ticks: {stepSize: 0.5}
+                    }
+                },
+                animation: {
+                    duration: 0
+                }
+            }
+        });
+    } else $("span.warn").show();
+    
+}
 
 function simulate(h0, v0, theta0, end_fcn) {
     var t = 0;
